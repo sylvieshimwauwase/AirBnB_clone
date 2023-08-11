2c6c491e458b8cb583a2d41e9404cdbb1fc9fc2c
Tituz175 <tobititus01@gmail.com>
2023-08-10 22:16:59 -0600

    task 5 finished in tobi's branch

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This is an AirBnB Python package having the Base Model class defined"""
 import uuid
 from datetime import datetime
-# from models import storage
+import models
 
 
 class BaseModel:
@@ -21,7 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """The string representation of the Base Model class"""
@@ -30,7 +30,7 @@ class BaseModel:
     def save(self):
         """This method saves the object attributes to the storage"""
         self.updated_at = datetime.now()
-        # storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """This method convert the object attributes to a dictionary"""
